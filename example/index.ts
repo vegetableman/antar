fe27d624dec3a9d8d2bbf3947ac6906fa6c0da36
vignesh anand <vigneshanand17@gmail.com>
2019-05-16 22:10:32 +0530

    Fixed example load handler

--- example/index.ts
+++ example/index.ts
@@ -1,23 +1,24 @@
 import antar from "../index";
 const page1 = document.createElement("iframe");
 page1.src = "page1.html";
+page1.onload = function() {
+  const page2 = document.createElement("iframe");
+  page2.src = "page2.html";
+  page2.onload = function() {
+    const output = antar.diffDocument(
+      page1.contentDocument,
+      page2.contentDocument,
+      {
+        output: "json",
+        enableScore: true
+      }
+    );
+    console.log(output);
+  };
+  document.body.appendChild(page2);
+};
 document.body.appendChild(page1);
 
-const page2 = document.createElement("iframe");
-page2.src = "page2.html";
-page2.onload = function() {
-  const output = antar.diffDocument(
-    page1.contentDocument,
-    page2.contentDocument,
-    {
-      output: "json",
-      enableScore: true
-    }
-  );
-  console.log(output);
-};
-
-document.body.appendChild(page2);
 console.log("a b c", "|", "a c");
 console.log(antar.diff("a b c", "a c"));
 console.log("-------");
