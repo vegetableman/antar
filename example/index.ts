9edbdb23dc29b9eb2bb7ac8f996e28e5dd606ae9
vignesh anand <vigneshanand17@gmail.com>
2019-05-18 23:57:29 +0530

    Added overlay

--- example/index.ts
+++ example/index.ts
@@ -1,21 +1,39 @@
 import antar from "../index";
 const page1 = document.createElement("iframe");
 page1.src = "page1.html";
+let output;
 page1.onload = function() {
   const page2 = document.createElement("iframe");
   page2.src = "page2.html";
   page2.onload = function() {
-    const output = antar.diffDocument(
-      page1.contentDocument,
-      page2.contentDocument,
-      {
-        output: "json",
-        enableScore: true
-      }
-    );
+    output = antar.diffDocument(page1.contentDocument, page2.contentDocument, {
+      output: "json",
+      enableScore: true
+    });
     console.log(output);
+
+    const html = antar.diffDocument(
+      page1.contentDocument,
+      page2.contentDocument
+    );
+    document.getElementsByClassName("diff-output")[0].innerHTML = <string>html;
   };
   document.body.appendChild(page2);
+  document
+    .getElementsByClassName("diff-btn")[0]
+    .addEventListener("click", function() {
+      (<HTMLElement>(
+        document.getElementsByClassName("diff-overlay")[0]
+      )).style.display = "block";
+      console.log("output: ", output);
+    });
+  document
+    .getElementsByClassName("diff-overlay-close")[0]
+    .addEventListener("click", function() {
+      (<HTMLElement>(
+        document.getElementsByClassName("diff-overlay")[0]
+      )).style.display = "none";
+    });
 };
 document.body.appendChild(page1);
 
